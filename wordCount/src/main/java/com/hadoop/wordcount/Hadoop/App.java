36a30200da85036f00e7c51bc0548d458b60434f
Avinash <sreethalam.avinash@gmail.com>
2018-02-27 08:55:29 +0530

    Add files via upload

---
+++ wordCount/src/main/java/com/hadoop/wordcount/Hadoop/App.java
@@ -0,0 +1,13 @@
+package com.hadoop.wordcount.Hadoop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
